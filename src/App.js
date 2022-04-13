eb68ade440ab8da88887c18dd1d599202d66cc1d
Gokalp Giray <gokalpgiray24@gmail.com>
2022-04-14 00:58:40 +0300

    gg-2 Değişken isimleri düzenlendi

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { useEffect, useState } from 'react'
 import axios from 'axios';
 
 function App() {
-  const [users, setUsers] = useState(null);
+  const [weather, setWeather] = useState(null);
 
   const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=metric&exclude=hourly,current,minutely,alerts&appid=af3eda7c637910e6048fe05760c13f9d';
 
-  const searchLocation = (e) => {
+  const getWeather = (e) => {
     axios.get(url).then((response) => {
-      setUsers(response.data)
+      setWeather(response.data)
       // console.log(response.data.daily[0].temp.day);
       // console.log(response.data.daily[0].temp.night);
       // console.log(response.data.daily[0].weather[0].icon);
@@ -19,14 +19,14 @@ function App() {
   // console.log(users.daily[0]);
 
   useEffect(() => {
-    console.log(users?.daily[0].temp.day);
-    console.log(users?.daily[0].temp.night);
-    console.log(users?.daily[0].weather[0].icon);
-  }, [users]);
+    console.log(weather?.daily[0].temp.day);
+    console.log(weather?.daily[0].temp.night);
+    console.log(weather?.daily[0].weather[0].icon);
+  }, [weather]);
 
   return (
     <div className="App">
-      <button onClick={e => searchLocation(e)}>Click</button>
+      <button onClick={e => getWeather(e)}>Click</button>
     </div>
   );
 }
