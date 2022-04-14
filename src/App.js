fa705d343e14ea639adc5cd5e729b99cf444dce9
Gokalp Giray <gokalpgiray24@gmail.com>
2022-04-14 23:04:25 +0300

    gg-4 Data düzenlemesi yapıldı.

--- src/App.js
+++ src/App.js
@@ -3,336 +3,336 @@ import { useEffect, useState } from 'react'
 import axios from 'axios';
 
 const day = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+const city = {
+  "cityname": [
+    {
+      "name": "Adana",
+      "nameEn": "Adana"
+    },
+    {
+      "name": "Adıyaman",
+      "nameEn": "Adiyaman"
+    },
+    {
+      "name": "Afyonkarahisar",
+      "nameEn": "Afyonkarahisar"
+    },
+    {
+      "name": "Ağrı",
+      "nameEn": "Agri"
+    },
+    {
+      "name": "Amasya",
+      "nameEn": "Amasya"
+    },
+    {
+      "name": "Ankara",
+      "nameEn": "Ankara"
+    },
+    {
+      "name": "Antalya",
+      "nameEn": "Antalya"
+    },
+    {
+      "name": "Artvin",
+      "nameEn": "Artvin"
+    },
+    {
+      "name": "Aydın",
+      "nameEn": "Aydin"
+    },
+    {
+      "name": "Balıkesir",
+      "nameEn": "Balikesir"
+    },
+    {
+      "name": "Bilecik",
+      "nameEn": "Bilecik"
+    },
+    {
+      "name": "Bingöl",
+      "nameEn": "Bingol"
+    },
+    {
+      "name": "Bitlis",
+      "nameEn": "Bitlis"
+    },
+    {
+      "name": "Bolu",
+      "nameEn": "Bolu"
+    },
+    {
+      "name": "Burdur",
+      "nameEn": "Burdur"
+    },
+    {
+      "name": "Bursa",
+      "nameEn": "Bursa"
+    },
+    {
+      "name": "Çanakkale",
+      "nameEn": "Canakkale"
+    },
+    {
+      "name": "Çankırı",
+      "nameEn": "Cankırı"
+    },
+    {
+      "name": "Çorum",
+      "nameEn": "Corum"
+    },
+    {
+      "name": "Denizli",
+      "nameEn": "Denizli"
+    },
+    {
+      "name": "Diyarbakır",
+      "nameEn": "Diyarbakir"
+    },
+    {
+      "name": "Edirne",
+      "nameEn": "Edirne"
+    },
+    {
+      "name": "Elazığ",
+      "nameEn": "Elazig"
+    },
+    {
+      "name": "Erzincan",
+      "nameEn": "Erzincan"
+    },
+    {
+      "name": "Erzurum",
+      "nameEn": "Erzurum"
+    },
+    {
+      "name": "Eskişehir",
+      "nameEn": "Eskisehir"
+    },
+    {
+      "name": "Gaziantep",
+      "nameEn": "Gaziantep"
+    },
+    {
+      "name": "Giresun",
+      "nameEn": "Giresun"
+    },
+    {
+      "name": "Gümüşhane",
+      "nameEn": "Gumushane"
+    },
+    {
+      "name": "Hakkari",
+      "nameEn": "Hakkari"
+    },
+    {
+      "name": "Hatay",
+      "nameEn": "Hatay"
+    },
+    {
+      "name": "Isparta",
+      "nameEn": "Isparta"
+    },
+    {
+      "name": "Mersin",
+      "nameEn": "Mersin"
+    },
+    {
+      "name": "İstanbul",
+      "nameEn": "Istanbul"
+    },
+    {
+      "name": "İzmir",
+      "nameEn": "Izmir"
+    },
+    {
+      "name": "Kars",
+      "nameEn": "Kars"
+    },
+    {
+      "name": "Kastamonu",
+      "nameEn": "Kastamonu"
+    },
+    {
+      "name": "Kayseri",
+      "nameEn": "Kayseri"
+    },
+    {
+      "name": "Kırklareli",
+      "nameEn": "Kirklareli"
+    },
+    {
+      "name": "Kırşehir",
+      "nameEn": "Kirşehir"
+    },
+    {
+      "name": "Kocaeli",
+      "nameEn": "Kocaeli"
+    },
+    {
+      "name": "Konya",
+      "nameEn": "Konya"
+    },
+    {
+      "name": "Kütahya",
+      "nameEn": "Kutahya"
+    },
+    {
+      "name": "Malatya",
+      "nameEn": "Malatya"
+    },
+    {
+      "name": "Manisa",
+      "nameEn": "Manisa"
+    },
+    {
+      "name": "Kahramanmaraş",
+      "nameEn": "Kahramanmaras"
+    },
+    {
+      "name": "Mardin",
+      "nameEn": "Mardin"
+    },
+    {
+      "name": "Muğla",
+      "nameEn": "Mugla"
+    },
+    {
+      "name": "Muş",
+      "nameEn": "Mus"
+    },
+    {
+      "name": "Nevşehir",
+      "nameEn": "Nevsehir"
+    },
+    {
+      "name": "Niğde",
+      "nameEn": "Nigde"
+    },
+    {
+      "name": "Ordu",
+      "nameEn": "Ordu"
+    },
+    {
+      "name": "Rize",
+      "nameEn": "Rize"
+    },
+    {
+      "name": "Sakarya",
+      "nameEn": "Sakarya"
+    },
+    {
+      "name": "Samsun",
+      "nameEn": "Samsun"
+    },
+    {
+      "name": "Siirt",
+      "nameEn": "Siirt"
+    },
+    {
+      "name": "Sinop",
+      "nameEn": "Sinop"
+    },
+    {
+      "name": "Sivas",
+      "nameEn": "Sivas"
+    },
+    {
+      "name": "Tekirdağ",
+      "nameEn": "Tekirdag"
+    },
+    {
+      "name": "Tokat",
+      "nameEn": "Tokat"
+    },
+    {
+      "name": "Trabzon",
+      "nameEn": "Trabzon"
+    },
+    {
+      "name": "Tunceli",
+      "nameEn": "Tunceli"
+    },
+    {
+      "name": "Şanlıurfa",
+      "nameEn": "Sanliurfa"
+    },
+    {
+      "name": "Uşak",
+      "nameEn": "Usak"
+    },
+    {
+      "name": "Van",
+      "nameEn": "Van"
+    },
+    {
+      "name": "Yozgat",
+      "nameEn": "Yozgat"
+    },
+    {
+      "name": "Zonguldak",
+      "nameEn": "Zonguldak"
+    },
+    {
+      "name": "Aksaray",
+      "nameEn": "Aksaray"
+    },
+    {
+      "name": "Bayburt",
+      "nameEn": "Bayburt"
+    },
+    {
+      "name": "Karaman",
+      "nameEn": "Karaman"
+    },
+    {
+      "name": "Kırıkkale",
+      "nameEn": "Kirikkale"
+    },
+    {
+      "name": "Batman",
+      "nameEn": "Batman"
+    },
+    {
+      "name": "Şırnak",
+      "nameEn": "Sirnak"
+    },
+    {
+      "name": "Bartın",
+      "nameEn": "Bartin"
+    },
+    {
+      "name": "Ardahan",
+      "nameEn": "Ardahan"
+    },
+    {
+      "name": "Iğdır",
+      "nameEn": "Igdir"
+    },
+    {
+      "name": "Yalova",
+      "nameEn": "Yalova"
+    },
+    {
+      "name": "Karabük",
+      "nameEn": "Karabuk"
+    },
+    {
+      "name": "Kilis",
+      "nameEn": "Kilis"
+    },
+    {
+      "name": "Osmaniye",
+      "nameEn": "Osmaniye"
+    },
+    {
+      "name": "Düzce",
+      "nameEn": "Duzce"
+    }
+  ]
+};
 
 function App() {
-  const [city, setCity] = useState({
-    "cityname": [
-      {
-        "name": "Adana",
-        "nameEn": "Adana"
-      },
-      {
-        "name": "Adıyaman",
-        "nameEn": "Adiyaman"
-      },
-      {
-        "name": "Afyonkarahisar",
-        "nameEn": "Afyonkarahisar"
-      },
-      {
-        "name": "Ağrı",
-        "nameEn": "Agri"
-      },
-      {
-        "name": "Amasya",
-        "nameEn": "Amasya"
-      },
-      {
-        "name": "Ankara",
-        "nameEn": "Ankara"
-      },
-      {
-        "name": "Antalya",
-        "nameEn": "Antalya"
-      },
-      {
-        "name": "Artvin",
-        "nameEn": "Artvin"
-      },
-      {
-        "name": "Aydın",
-        "nameEn": "Aydin"
-      },
-      {
-        "name": "Balıkesir",
-        "nameEn": "Balikesir"
-      },
-      {
-        "name": "Bilecik",
-        "nameEn": "Bilecik"
-      },
-      {
-        "name": "Bingöl",
-        "nameEn": "Bingol"
-      },
-      {
-        "name": "Bitlis",
-        "nameEn": "Bitlis"
-      },
-      {
-        "name": "Bolu",
-        "nameEn": "Bolu"
-      },
-      {
-        "name": "Burdur",
-        "nameEn": "Burdur"
-      },
-      {
-        "name": "Bursa",
-        "nameEn": "Bursa"
-      },
-      {
-        "name": "Çanakkale",
-        "nameEn": "Canakkale"
-      },
-      {
-        "name": "Çankırı",
-        "nameEn": "Cankırı"
-      },
-      {
-        "name": "Çorum",
-        "nameEn": "Corum"
-      },
-      {
-        "name": "Denizli",
-        "nameEn": "Denizli"
-      },
-      {
-        "name": "Diyarbakır",
-        "nameEn": "Diyarbakir"
-      },
-      {
-        "name": "Edirne",
-        "nameEn": "Edirne"
-      },
-      {
-        "name": "Elazığ",
-        "nameEn": "Elazig"
-      },
-      {
-        "name": "Erzincan",
-        "nameEn": "Erzincan"
-      },
-      {
-        "name": "Erzurum",
-        "nameEn": "Erzurum"
-      },
-      {
-        "name": "Eskişehir",
-        "nameEn": "Eskisehir"
-      },
-      {
-        "name": "Gaziantep",
-        "nameEn": "Gaziantep"
-      },
-      {
-        "name": "Giresun",
-        "nameEn": "Giresun"
-      },
-      {
-        "name": "Gümüşhane",
-        "nameEn": "Gumushane"
-      },
-      {
-        "name": "Hakkari",
-        "nameEn": "Hakkari"
-      },
-      {
-        "name": "Hatay",
-        "nameEn": "Hatay"
-      },
-      {
-        "name": "Isparta",
-        "nameEn": "Isparta"
-      },
-      {
-        "name": "Mersin",
-        "nameEn": "Mersin"
-      },
-      {
-        "name": "İstanbul",
-        "nameEn": "Istanbul"
-      },
-      {
-        "name": "İzmir",
-        "nameEn": "Izmir"
-      },
-      {
-        "name": "Kars",
-        "nameEn": "Kars"
-      },
-      {
-        "name": "Kastamonu",
-        "nameEn": "Kastamonu"
-      },
-      {
-        "name": "Kayseri",
-        "nameEn": "Kayseri"
-      },
-      {
-        "name": "Kırklareli",
-        "nameEn": "Kirklareli"
-      },
-      {
-        "name": "Kırşehir",
-        "nameEn": "Kirşehir"
-      },
-      {
-        "name": "Kocaeli",
-        "nameEn": "Kocaeli"
-      },
-      {
-        "name": "Konya",
-        "nameEn": "Konya"
-      },
-      {
-        "name": "Kütahya",
-        "nameEn": "Kutahya"
-      },
-      {
-        "name": "Malatya",
-        "nameEn": "Malatya"
-      },
-      {
-        "name": "Manisa",
-        "nameEn": "Manisa"
-      },
-      {
-        "name": "Kahramanmaraş",
-        "nameEn": "Kahramanmaras"
-      },
-      {
-        "name": "Mardin",
-        "nameEn": "Mardin"
-      },
-      {
-        "name": "Muğla",
-        "nameEn": "Mugla"
-      },
-      {
-        "name": "Muş",
-        "nameEn": "Mus"
-      },
-      {
-        "name": "Nevşehir",
-        "nameEn": "Nevsehir"
-      },
-      {
-        "name": "Niğde",
-        "nameEn": "Nigde"
-      },
-      {
-        "name": "Ordu",
-        "nameEn": "Ordu"
-      },
-      {
-        "name": "Rize",
-        "nameEn": "Rize"
-      },
-      {
-        "name": "Sakarya",
-        "nameEn": "Sakarya"
-      },
-      {
-        "name": "Samsun",
-        "nameEn": "Samsun"
-      },
-      {
-        "name": "Siirt",
-        "nameEn": "Siirt"
-      },
-      {
-        "name": "Sinop",
-        "nameEn": "Sinop"
-      },
-      {
-        "name": "Sivas",
-        "nameEn": "Sivas"
-      },
-      {
-        "name": "Tekirdağ",
-        "nameEn": "Tekirdag"
-      },
-      {
-        "name": "Tokat",
-        "nameEn": "Tokat"
-      },
-      {
-        "name": "Trabzon",
-        "nameEn": "Trabzon"
-      },
-      {
-        "name": "Tunceli",
-        "nameEn": "Tunceli"
-      },
-      {
-        "name": "Şanlıurfa",
-        "nameEn": "Sanliurfa"
-      },
-      {
-        "name": "Uşak",
-        "nameEn": "Usak"
-      },
-      {
-        "name": "Van",
-        "nameEn": "Van"
-      },
-      {
-        "name": "Yozgat",
-        "nameEn": "Yozgat"
-      },
-      {
-        "name": "Zonguldak",
-        "nameEn": "Zonguldak"
-      },
-      {
-        "name": "Aksaray",
-        "nameEn": "Aksaray"
-      },
-      {
-        "name": "Bayburt",
-        "nameEn": "Bayburt"
-      },
-      {
-        "name": "Karaman",
-        "nameEn": "Karaman"
-      },
-      {
-        "name": "Kırıkkale",
-        "nameEn": "Kirikkale"
-      },
-      {
-        "name": "Batman",
-        "nameEn": "Batman"
-      },
-      {
-        "name": "Şırnak",
-        "nameEn": "Sirnak"
-      },
-      {
-        "name": "Bartın",
-        "nameEn": "Bartin"
-      },
-      {
-        "name": "Ardahan",
-        "nameEn": "Ardahan"
-      },
-      {
-        "name": "Iğdır",
-        "nameEn": "Igdir"
-      },
-      {
-        "name": "Yalova",
-        "nameEn": "Yalova"
-      },
-      {
-        "name": "Karabük",
-        "nameEn": "Karabuk"
-      },
-      {
-        "name": "Kilis",
-        "nameEn": "Kilis"
-      },
-      {
-        "name": "Osmaniye",
-        "nameEn": "Osmaniye"
-      },
-      {
-        "name": "Düzce",
-        "nameEn": "Duzce"
-      }
-    ]
-  });
   const [weather, setWeather] = useState(null);
   const [selectedCity, setSelectedCity] = useState("Istanbul");
 
@@ -340,9 +340,9 @@ function App() {
     getCoords();
   }, []);
 
-  const appid = ["6c9498545f0b7e313acac5a7166677ea", "af3eda7c637910e6048fe05760c13f9d", "76408e74d198f03f5d26f16eacb33d1f"]; 
+  const appid = [""]; // Buraya Open Weather Map'ten alacağınız api key'ini girmeniz gerekiyor.
 
-  const urlFirst = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity},tr&appid=${appid[2]}`
+  const urlFirst = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity},tr&appid=${appid[0]}`
 
   const getCoords = () => {
     axios.get(urlFirst).then((response) => {
@@ -351,7 +351,7 @@ function App() {
   };
   
   const getWeather = (data) => {
-    const urlSecond = `https://api.openweathermap.org/data/2.5/onecall?lat=${data?.coord.lat}&lon=${data?.coord.lon}&units=metric&exclude=hourly,current,minutely,alerts&appid=${appid[2]}`;
+    const urlSecond = `https://api.openweathermap.org/data/2.5/onecall?lat=${data?.coord.lat}&lon=${data?.coord.lon}&units=metric&exclude=hourly,current,minutely,alerts&appid=${appid[0]}`;
     axios.get(urlSecond).then((response) => {
       setWeather(response.data)
     });
